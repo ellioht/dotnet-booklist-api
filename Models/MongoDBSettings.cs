9df6668627779017d7c95ab4efa3cf6c0d5f845e
Elliot <130664947+ellioht@users.noreply.github.com>
2023-08-29 23:55:44 +0100

    mongo

--- Models/MongoDBSettings.cs
+++ Models/MongoDBSettings.cs
@@ -1,9 +1,8 @@
-namespace Models
+namespace MongoBookAPI.Models;
+
+public class MongoDBSettings
 {
-  public class MongoDBSettings
-  {
-    public string ConnectionURI { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string CollectionName { get; set; } = null!;
-  }
+  public string ConnectionURI { get; set; } = null!;
+  public string DatabaseName { get; set; } = null!;
+  public string CollectionName { get; set; } = null!;
 }
